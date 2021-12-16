7789ea9a28dce4ce45fbb68daa4b8984200b20f8
Israr <israr.codeilm@gmail.com>
2021-12-16 12:13:07 +0530

    Changed the App location

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+# from app.api import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run(app='app.api:app', host="127.0.0.1", port=8080, reload=True)
